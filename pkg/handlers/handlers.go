674dfbe1385e055a1d18b149fd42b31d1d51a589
CongSanhLe <congsanhle1999@gmail.com>
2022-12-21 02:39:05 +0700

    [21122022] config template when refresh load data

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,13 +3,30 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/tsawler/go-course/pkg/config"
 	"github.com/tsawler/go-course/pkg/render"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.html")
 }
 
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.html")
 }
