ba20f4f1a515b959e84f129530b4b22408282a09
CongSanhLe <congsanhle1999@gmail.com>
2022-12-21 01:22:28 +0700

    [21122022] template cache

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,16 +1,81 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
+var functions = template.FuncMap{}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+
+	tc, err := CreateTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+
+	t.Execute(buf, nil)
+
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser", err)
+	}
+
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+
+	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("err parsed template", err)
 		return
 	}
+}
+
+// CreateTemplateCache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+	// get all of pages
+	// all of things in this template folder
+	pages, err := filepath.Glob("./templates/*.html")
+	if err != nil {
+		return myCache, err
+	}
+	for _, page := range pages {
+		// 2 thing happen
+		// + every page find --> get the index and first time though will be zero
+		// --> get the actual page itself
+		//
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		// find template match layout
+		matches, err := filepath.Glob("./templates/*.layout.html")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.html")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = ts
+
+	}
+	return myCache, nil
 }
